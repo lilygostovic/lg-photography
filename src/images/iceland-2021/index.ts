e2ae65d33c0076f9e13d3bf8173516384ac55327
lilygostovic <lily.gostovic@gmail.com>
2023-10-15 19:30:23 -0400

    Try fix build

--- src/images/iceland-2021/index.ts
+++ src/images/iceland-2021/index.ts
@@ -1,16 +1,16 @@
 // todo:: order images properly
 
-import img0 from './0.jpg';
-import img1 from './1.jpg';
-import img2 from './2.jpg';
-import img3 from './3.jpg';
-import cover from './cover.jpg';
-import imgIMG_8445 from './IMG_8445.jpg';
-import imgIMG_8606 from './IMG_8606.jpg';
-import imgIMG_8613 from './IMG_8613.jpg';
-import imgIMG_9076 from './IMG_9076.jpg';
-import imgIMG_9179 from './IMG_9179.jpg';
-import imgIMG_9329 from './IMG_9329.jpg';
+import img0 from "./0.jpg";
+import img1 from "./1.jpg";
+import img2 from "./2.jpg";
+import img3 from "./3.jpg";
+import cover from "./cover.jpg";
+import imgIMG_8445 from "./IMG_8445.jpg";
+import imgIMG_8606 from "./IMG_8606.jpg";
+import imgIMG_8613 from "./IMG_8613.jpg";
+import imgIMG_9076 from "./IMG_9076.jpg";
+import imgIMG_9179 from "./IMG_9179.jpg";
+import imgIMG_9329 from "./IMG_9329.jpg";
 
 const images = [
   cover,
