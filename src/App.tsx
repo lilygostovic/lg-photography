b5a2514f5691c78d1e5db9939cc49b989f16c8fb
lilygostovic <lily.gostovic@gmail.com>
2023-10-17 11:00:52 -0400

    Remove simple route

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import {
-  Route,
-  Routes,
-} from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 
-import {
-  HomePage,
-  JournalPage,
-} from './components';
+import { HomePage, JournalPage } from "./components";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/journal/:id" element={<JournalPage />} />
-      <Route path="/lily" element={<div>hi my name is lily!</div>} />
     </Routes>
   );
 }
