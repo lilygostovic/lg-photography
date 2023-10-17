628b9569376e6f8bf7186f795bff4d729470e7ff
lilygostovic <lily.gostovic@gmail.com>
2023-10-17 11:16:21 -0400

    Add camera type to journals

--- src/components/JournalPage.tsx
+++ src/components/JournalPage.tsx
@@ -1,11 +1,11 @@
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
-import journals from '../journals/journals.json';
-import { StyledDiv } from './common/StyledDiv';
-import { StyledText } from './common/StyledText';
+import journals from "../journals/journals.json";
+import { StyledDiv } from "./common/StyledDiv";
+import { StyledText } from "./common/StyledText";
 
 export const JournalPage = () => {
-  const {id} = useParams();
+  const { id } = useParams();
 
   const journal = journals.find((journal) => journal.id === id);
 
@@ -24,7 +24,9 @@ export const JournalPage = () => {
         <StyledText variant="subtitle">Lily Gostovic — Journal</StyledText>
       </a>
       <StyledText variant="journalTitle">{journal.title}</StyledText>
-      <StyledText variant="journalSubtitle">{journal.date}</StyledText>
+      <StyledText variant="journalSubtitle">
+        {journal.date} | {journal.camera}
+      </StyledText>
       {journal.images.map((image) => (
         <StyledDiv height="1050px" width="1050px" my="40px">
           <img
