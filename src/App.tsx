32d4f2a83ab10a864faa67fc65290a582cdd5741
lilygostovic <lily.gostovic@gmail.com>
2023-10-17 10:46:05 -0400

    Try fix broken paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import {
-  BrowserRouter,
   Route,
   Routes,
 } from 'react-router-dom';
@@ -13,12 +12,24 @@ import {
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/journal/:id" element={<JournalPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      {/* <Route path="/journal/:id" element={<JournalPage />} /> */}
+      <Route path="/journal/alberta-2023" element={<JournalPage id="alberta-2023"/>} />
+      <Route path="/journal/christmas-2022" element={<JournalPage id="christmas-2022"/>} />
+      <Route path="/journal/iceland-2021" element={<JournalPage id="iceland-2021"/>} />
+      <Route path="/journal/new-york-city-2022" element={<JournalPage id="new-york-city-2022"/>} />
+      <Route path="/journal/summer-2021" element={<JournalPage id="summer-2021"/>} />
+      <Route path="/journal/christmas-2021" element={<JournalPage id="christmas-2021"/>} />
+      <Route path="/journal/alberta-2020" element={<JournalPage id="alberta-2020"/>} />
+      <Route path="/journal/new-york-city" element={<JournalPage id="new-york-city"/>} />
+      <Route path="/journal/san-francisco" element={<JournalPage id="san-francisco"/>} />
+      <Route path="/journal/paris-2018" element={<JournalPage id="paris-2018"/>} />
+      <Route path="/journal/dijon" element={<JournalPage id="dijon"/>} />
+      <Route path="/journal/loire-valley" element={<JournalPage id="loire-valley"/>} />
+      <Route path="/journal/monflanquin" element={<JournalPage id="monflanquin"/>} />
+      <Route path="/journal/bordeaux" element={<JournalPage id="bordeaux"/>} />
+    </Routes>
   );
 }
 
--- src/components/JournalPage.tsx
+++ src/components/JournalPage.tsx
@@ -1,13 +1,15 @@
-import { useParams } from "react-router-dom";
+import journals from '../journals/journals.json';
+import { JournalID } from '../types/id';
+import { StyledDiv } from './common/StyledDiv';
+import { StyledText } from './common/StyledText';
 
-import journals from "../journals/journals.json";
-import { StyledDiv } from "./common/StyledDiv";
-import { StyledText } from "./common/StyledText";
+type JournalPageProps = {
+  id: JournalID;
+}
 
-export const JournalPage = () => {
-  const { id } = useParams();
+export const JournalPage = ({id}: JournalPageProps) => {
+  // const { id } = useParams();
 
-  console.log(journals);
   const journal = journals.find((journal) => journal.id === id);
 
   if (!journal) {
