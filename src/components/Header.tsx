b5d9b17b39c63bce7759847df433436ea92eec0a
lilygostovic <lily.gostovic@gmail.com>
2025-03-17 23:25:20 +0000

    feat: add loading state for images

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,48 @@
+import { useEffect, useState } from "react";
 import { StyledDiv } from "./common/StyledDiv";
 import { StyledText } from "./common/StyledText";
+import Skeleton from "react-loading-skeleton";
 
+/** The URI of the headshot image. */
 const HEADSHOT_URI =
   "https://res.cloudinary.com/dd4gz3mqg/image/upload/v1740914458/profile-picture_zc5m15.jpg";
+/** The diameter of the headshot image. */
+const HEADSHOT_DIAMETER = 150;
+/** The border radius of the headshot image. */
+const HEADSHOT_BORDER_RADIUS = 150;
 
-export const Header = () => (
-  <StyledDiv
-    display="flex"
-    flexDirection="column"
-    alignItems="center"
-    mt="80px"
-    mb="80px"
-  >
-    <img
-      src={HEADSHOT_URI}
-      alt="headshot"
-      style={{ height: "90px", borderRadius: "150px" }}
-    />
-    <StyledText variant="title">Lily Gostovic</StyledText>
-    <StyledText variant="subtitle">
-      Minolta Riva Zoom 125EX | Olympus OM-D EM-5 III
-    </StyledText>
-  </StyledDiv>
-);
+export const Header = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = HEADSHOT_URI;
+    img.onload = () => {
+      setIsLoaded(true);
+    };
+  }, []);
+  
+  return (
+    <StyledDiv
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      mt="80px"
+      mb="80px"
+    >
+      {isLoaded ? (
+        <img
+          src={HEADSHOT_URI}
+          alt="headshot"
+          style={{ height: HEADSHOT_DIAMETER, borderRadius: HEADSHOT_BORDER_RADIUS }}
+        />
+      ): (
+        <Skeleton height={HEADSHOT_DIAMETER} width={HEADSHOT_DIAMETER} borderRadius={HEADSHOT_BORDER_RADIUS} />
+      )}
+      <StyledText variant="title">Lily Gostovic</StyledText>
+      <StyledText variant="subtitle">
+        Minolta Riva Zoom 125EX | Olympus OM-D EM-5 III
+      </StyledText>
+    </StyledDiv>
+  );
+};
--- src/components/JournalPage/JournalImage.tsx
+++ src/components/JournalPage/JournalImage.tsx
@@ -1,18 +1,25 @@
 import { useEffect, useRef, useState } from "react";
 import { StyledDiv } from "../common/StyledDiv";
+import 'react-loading-skeleton/dist/skeleton.css'
+import Skeleton from "react-loading-skeleton";
 
 /** The maximum width of a journal image. */
 const IMAGE_WIDTH = 850;
 /** The margin below a journal image. */
 const IMAGE_MARGIN_BOTTOM = 60;
+/** The width of the loading image. */
+const LOADING_IMAGE_WIDTH = IMAGE_WIDTH;
+/** The height of the loading image. */
+const LOADING_IMAGE_HEIGHT = 550;
 
 /**
  * A component that displays a journal image. Dimensions are calculated based on the image's aspect ratio.
  * @param image - The image to display.
  */
 export const JournalImage = ({ image }: { image: string }) => {
-  const [dimensions, setDimensions] = useState({ width: IMAGE_WIDTH, height: IMAGE_WIDTH });
+  const [dimensions, setDimensions] = useState({ width: LOADING_IMAGE_WIDTH, height: LOADING_IMAGE_HEIGHT });
   const imgRef = useRef(null);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     const img = new Image();
@@ -24,24 +31,31 @@ export const JournalImage = ({ image }: { image: string }) => {
         const aspectRatio = img.width / img.height;
         const calculatedHeight = IMAGE_WIDTH / aspectRatio;
         setDimensions({ width: IMAGE_WIDTH, height: calculatedHeight });
+      } else {
+        setDimensions({ width: IMAGE_WIDTH, height: IMAGE_WIDTH });
       }
+      setIsLoaded(true);
     };
   }, [image]);
 
   return (
     <StyledDiv 
-      width={`${IMAGE_WIDTH}px`}
+      width={`${dimensions.width}px`}
       height={`${dimensions.height}px`} 
       my={IMAGE_MARGIN_BOTTOM}
     >
-      <img
-        ref={imgRef}
-        src={image}
-        alt={image}
-        height="100%"
-        width="100%"
-        style={{ objectFit: "contain" }}
-      />
+      {isLoaded ? (
+        <img
+          ref={imgRef}
+          src={image}
+          alt={image}
+          height="100%"
+          width="100%"
+          style={{ objectFit: "contain" }}
+        />
+      ) : (
+          <Skeleton count={1} height={`${dimensions.height}px`} width={`${dimensions.width}px`} />
+      )}
     </StyledDiv>
   );
 };
